a69f4b7039a832460a3e4705ce9cb5f663ed021f
vincent peng <viper404@163.com>
2017-02-08 23:50:39 +0800

    Add url-GET

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const Koa = require('koa');
 
+const router = require('koa-router')();
+
 const app = new Koa();
 
 app.use(async (ctx, next) => {
-	console.log(`${ctx.request.method} ${ctx.request.url}`);
+	console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
 	await next();
-})
-
-app.use(async (ctx, next) => {
-	const start = new Date().getTime();
-	await next();
-	const ms = new Date().getTime() - start;
-	console.log(`Time: ${ms}ms`);
-})
-
-app.use(async (ctx, next) => {
-	await next();
-	ctx.response.type = 'text/html';
-	ctx.response.body = '<h1>Hello, koa2!</h1>';
 });
+
+router.get('/hello/:name', async (ctx, next) => {
+	var name = ctx.params.name;
+	ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+});
+
+router.get('/', async (ctx, next) => {
+	ctx.response.body = '<h1>Index</h1>';
+});
+
+app.use(router.routes());
 
 app.listen(3000);
 console.log('app started at port 3000...');
